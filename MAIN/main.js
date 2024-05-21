8a5698512f170eff04f92f88e58651ce488bff7c
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2024-05-21 23:32:11 +0300

    редизайн главной

--- MAIN/main.js
+++ MAIN/main.js
@@ -1,14 +1,13 @@
 const swiper = new Swiper('.swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    spaceBetween: 25,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: "auto",
-    slidesOffsetAfter: 25,
-    slidesOffsetBefore: 25,
-  });
-
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  spaceBetween: 80,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 'auto',
+  slidesOffsetAfter: 50,
+  slidesOffsetBefore: 25,
+});
