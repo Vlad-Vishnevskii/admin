d3e73248dccfacbba4906664c37953096b5f48f1
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2024-05-22 10:09:48 +0300

    iife

--- MAIN/main.js
+++ MAIN/main.js
@@ -1,13 +1,15 @@
-const swiper = new Swiper('.swiper', {
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  spaceBetween: 80,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  slidesPerView: 'auto',
-  slidesOffsetAfter: 50,
-  slidesOffsetBefore: 25,
-});
+(() => {
+  const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    spaceBetween: 80,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    slidesOffsetAfter: 50,
+    slidesOffsetBefore: 25,
+  });
+})();
